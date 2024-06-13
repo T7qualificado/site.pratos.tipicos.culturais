a3c600e5c558a0226aed4bea691576260de1103a
T7qualificado <106085191+T7qualificado@users.noreply.github.com>
2024-06-13 14:49:16 -0300

    ...

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.queryselector(".caixa-principal");
+const caixaPerguntas = document.queryselector(".caixa-perguntas");
+const caixaAlternativas = document.queryselector(".caixa-alternativas");
+const caixaResultado = document.queryselector(".caixa-resultado");
+const textoReultado = document.queryselector(".texto.resultado");
