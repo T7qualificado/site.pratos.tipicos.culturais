10f1329d9900d13f83639934cae4f50d15b2fc0d
T7qualificado <106085191+T7qualificado@users.noreply.github.com>
2024-07-03 13:42:10 -0300

    .

--- script.js
+++ script.js
@@ -6,24 +6,82 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-    enunciado: "Pergunta 1",
-    alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-    ]
-    },
-    {
-        enunciado: "Pergunta 2",
+        enunciado:  "o quiabo,o jiló "
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "",
+                afirmacao: " "
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
         ]
     },
     {
-        enunciado: "Pergunta 3",
+        enunciado: "",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "",
+                afirmacao: ""
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
         ]
-    }
+    },
+    {
+        enunciado: "",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: ""
+            },
+            {
+                texto: "",
+                afirmacao: ""
+            }
+        ]
+    },
 ];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
